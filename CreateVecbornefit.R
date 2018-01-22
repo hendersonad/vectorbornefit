1a933313f59e83565f3b23e5f40ca9c514514dbb
a-henderson91 <alasdair.henderson1@lshtm.ac.uk>
2018-01-22 10:02:06 +0000

    change wd commands

--- CreateVecbornefit.R
+++ CreateVecbornefit.R
@@ -2,20 +2,16 @@
 library("devtools")
 #devtools::install_github("klutometis/roxygen")
 library(roxygen2)
-
-setwd("/Users/AHenderson/Documents/R")
+getwd()
+setwd("../vectorbornefit")
 #create("vectorbornefit")
 
 # create documentation
-setwd("./vectorbornefit")
 document()
 
 # install
 setwd("..")
 install("vectorbornefit")
-
-#test
-
 
 ?MCMCloop_withtimevalsadj
 # install_github('vectorbornefit','a-henderson91')  
