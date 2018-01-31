5e2cc951fd222048d1c4381ea816b404700aedf0
a-henderson91 <alasdair.henderson1@lshtm.ac.uk>
2018-01-31 16:18:27 +0000

    Zika model - add chi priro unif(0,1)

--- R/fn_ComputePrior.R
+++ R/fn_ComputePrior.R
@@ -13,10 +13,12 @@ ComputePrior <- function(iiH, thetaAlltab, thetaAllstar){
 p_theta_star = priorInf(1/thetaAllstar[iiH,"Inf"])*priorExp(1/thetaAllstar[iiH,"Exp"])*
                 priorVEx(1/thetaAllstar[iiH,"Exp"])*priorMuV(1/thetaAllstar[iiH,"MuV"])*
                 priorBeta_amp(thetaAllstar[iiH,"beta_v_amp"])*priorBeta_mid(thetaAllstar[iiH,"beta_v_mid"])*
+                priorchi(thetaAllstar[iiH,"chi"])*
                 priort0(thetaAllstar[iiH,"t0"])
 p_theta = priorInf(1/thetaAlltab[iiH,"Inf"])*priorExp(1/thetaAlltab[iiH,"Exp"])*
             priorVEx(1/thetaAlltab[iiH,"Exp"])*priorMuV(1/thetaAlltab[iiH,"MuV"])*
             priorBeta_amp(thetaAlltab[iiH,"beta_v_amp"])*priorBeta_mid(thetaAlltab[iiH,"beta_v_mid"])*
+            priorchi(thetaAlltab[iiH,"chi"])*
             priort0(thetaAlltab[iiH,"t0"])
 
 names(p_theta_star)=NULL;names(p_theta)=NULL
--- R/fn_SampleTheta.R
+++ R/fn_SampleTheta.R
@@ -34,7 +34,11 @@ SampleTheta<-function(theta_in, theta_init_in, covartheta, covartheta_init, ages
         theta_star[["beta_h_2"]]=min(theta_star[["beta_h_2"]],2-theta_star[["beta_h_2"]]) # Ensure beta is between zero and 1
         theta_star[["beta_h_3"]]=min(theta_star[["beta_h_3"]],2-theta_star[["beta_h_3"]]) # Ensure beta is between zero and 1
       }
-  
+      
+      if(sum(names(theta_star)=="t0")>0){ # check theta contains this vector
+        theta_star[["t0"]]=min(theta_star[["t0"]],2-theta_star[["t0"]]) # Ensure start time between zero and 1 (therefore log is negative)
+      }
+      
     ## Initial conditions
     mean_vector_theta_init = theta_init_in
     
