ce20bc16ee352cd75f2dc5935501a9f5128a4e58
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2019-02-05 16:18:46 +0000

    INlcude vector control priors

--- R/fn_ComputePrior.R
+++ R/fn_ComputePrior.R
@@ -19,7 +19,7 @@ ComputePrior <- function(iiH, thetaAlltab, thetaAllstar, covartheta){
   # If vector control is included in the model - include prior density of proposed 
   #   strength of control measure
   if(vector.control==T){
-    p_theta_star <- p_theta_star*priorBeta_grad(thetaAllstar["beta_grad"])
+    p_theta_star <- p_theta_star*prior_control(c(thetaAllstar["beta_base"], thetaAllstar["beta_mid"]))
   }
   # If seasonal transmission is estimated, include in prior
   if(seasonal.transmission==T){
@@ -41,7 +41,7 @@ ComputePrior <- function(iiH, thetaAlltab, thetaAllstar, covartheta){
   # If vector control is included in the model - include prior density of proposed 
   #   strength of control measure
   if(vector.control==T){
-    p_theta <- p_theta*priorBeta_grad(thetaAlltab["beta_grad"])
+    p_theta <- p_theta*prior_control(c(thetaAlltab["beta_base"], thetaAlltab["beta_mid"]))
   }
   # If seasonal transmission is estimated, include in prior
   if(seasonal.transmission==T){
